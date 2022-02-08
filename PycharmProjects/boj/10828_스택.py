3a29f1af1858a07d62f665d61f77fbc889d42ab1
유성현 <31495131+SeongHyeon0409@users.noreply.github.com>
2022-02-09 00:34:31 +0900

    22.2.9

---
+++ PycharmProjects/boj/10828_스택.py
@@ -0,0 +1,46 @@
+import sys
+
+input = sys.stdin.readline
+
+def push(a, b):
+    a.append(b)
+
+def pop(a):
+    if len(a) == 0:
+        return -1
+    else:
+        c = a[len(a) - 1]
+        del a[len(a)-1]
+        return c
+
+def size(a):
+    return len(a)
+
+def empty(a):
+    if len(a) == 0:
+        return 1
+    else:
+        return 0
+
+def top(a):
+    if len(a) == 0:
+        return -1
+    else:
+        return a[len(a)-1]
+
+if __name__ == '__main__':
+    n = int(input())
+    answer = []
+    for i in range(n):
+        a = list(input().split())
+        if a[0] == "push":
+            push(answer, int(a[1]))
+        elif a[0] == "pop":
+            print(pop(answer))
+        elif a[0] == "size":
+            print(size(answer))
+        elif a[0] == "empty":
+            print(empty(answer))
+        else:
+            print(top(answer))
+
