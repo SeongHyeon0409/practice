7a00befc65898be708062152e13a1056f913f4d0
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-02-19 18:35:22 +0900

    이동

--- PycharmProjects/untitled2/1.py
+++
@@ -1,9 +0,0 @@
-x = int(input())
-
-n, m = map(int, input().split(" "))
-ary = []
-d = [10001] * (m+1)
-for i in range(n):
-    ary.append(int(input()))
-
-for i in range(g+1):
--- PycharmProjects/untitled2/tmep2.py
+++
@@ -1,6 +0,0 @@
-l,p = map(int,input().split())
-a =  list(map(int,input().split()))
-sum = l*p
-
-for i in range(5) :
-    print(a[i]-sum)
