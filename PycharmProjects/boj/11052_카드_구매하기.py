5c9e116a7e4cb6387ce3a80f9dee8e4715384823
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-02-21 16:19:48 +0900

    22.02.21 백준 알고리즘

---
+++ PycharmProjects/boj/11052_카드_구매하기.py
@@ -0,0 +1,13 @@
+n = int(input())
+cards = list(map(int, input().split()))
+
+dp = [0]
+for i in range(n):
+    dp.append(cards[i])
+
+
+for i in range(1, n+1):
+    for j in range(i, -1, -1):
+        dp[i] = max(dp[i], dp[i-j] + dp[j])
+
+print(dp[-1])
