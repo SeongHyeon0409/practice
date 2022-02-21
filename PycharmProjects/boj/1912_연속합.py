280fe596a9f161f345f297a9e7e2aae8336e8061
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-02-21 15:40:30 +0900

    22.02.21 백준 알고리즘

---
+++ PycharmProjects/boj/1912_연속합.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n= int(input().strip())
+nums = list(map(int, input().strip().split()))
+
+dp = []
+for i in range(n):
+    dp.append(nums[i])
+
+# 자기자신 or 바로전 최대값 + 자기자신 중 더 큰 값
+for i in range(1, n):
+    dp[i] = max(nums[i], nums[i] + dp[i-1])
+
+print(max(dp))
