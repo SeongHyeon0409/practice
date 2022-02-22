85dfcbdffb7367be3a07b79d328c506023f40040
SEONGHYEON\t1397 <t139754@gmail.com>
2022-02-22 16:16:26 +0900

    modify

--- PycharmProjects/boj/다이나믹/2579_계단오르기.py
+++ PycharmProjects/boj/다이나믹/2579_계단오르기.py
@@ -17,8 +17,6 @@ else:
     dp[1] = dp[0] + s[1]
     dp[2] = max(s[2] + s[1], s[0] + s[2])
     for i in range(3, n):
-
-
         dp[i] = s[i] + max(s[i-1] + dp[i-3], dp[i-2])
 
         #print("n :", i+1 , " , flag :", flag)
