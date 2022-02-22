e4c9d6aa6835ad2386b52aa31356985c9bb17fc5
SEONGHYEON\t1397 <t139754@gmail.com>
2022-02-22 16:16:12 +0900

    22.02.22 백준 알고리즘

---
+++ PycharmProjects/boj/다이나믹/2156_포도주시식.py
@@ -0,0 +1,18 @@
+n = int(input())
+g = [int(input()) for _ in range(n)]
+
+dp = [0] * n
+
+dp[0] = g[0]
+
+if n > 1:
+    dp[1] = g[0] + g[1]
+
+if n > 2:
+    dp[2] = max(dp[1], g[1]+g[2] ,g[0]+g[2])
+
+for i in range(3, n):
+    dp[i] = max(dp[i-2] + g[i], dp[i-1] , dp[i-3]+g[i-1]+g[i])
+
+print(dp)
+print(dp[-1])
