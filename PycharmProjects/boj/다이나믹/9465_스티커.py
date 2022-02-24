7dcc9b2622798491cfdca6c182f874666f567324
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-02-24 16:48:37 +0900

    22.02.24 백준 알고리즘

--- PycharmProjects/boj/다이나믹/9465_스티커.py
+++ PycharmProjects/boj/다이나믹/9465_스티커.py
@@ -12,12 +12,14 @@ for i in range(t):
 
     #printl(s)
 
-    dp = [([0] * n) for _ in range(2)]
+    dp = [([0] * (n+1)) for _ in range(2)]
 
     dp[0][0] = s[0][0]
     dp[1][0] = s[1][0]
-    dp[0][1] = dp[1][0] + s[0][1]
-    dp[1][1] = dp[0][0] + s[1][1]
+    
+    if n > 1:
+        dp[0][1] = dp[1][0] + s[0][1]
+        dp[1][1] = dp[0][0] + s[1][1]
 
     for j in range(2, n):
         dp[0][j] = max(s[0][j] + dp[1][j-1], s[0][j] + dp[1][j-2])
