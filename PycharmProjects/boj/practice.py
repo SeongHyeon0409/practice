208e30434144eaae36e1cc0e410d479803ed7320
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-03-05 21:43:38 +0900

    modify

--- PycharmProjects/boj/practice.py
+++ PycharmProjects/boj/practice.py
@@ -1,12 +1,1 @@
-ary = [7,3,2,1,5]
-
-for i in range(1, 5):
-    key = ary[i]
-    j = i
-    while  key < ary[j-1] and j > 0:
-        ary[j] = ary[j-1]
-        j -= 1
-    ary[j] = key
-
-print(ary)
-
+print("{0:09.3f}".format(3.12453))
