e0ae2082ac6e039ac84c460d67960b047e950b6c
SEONGHYEON\t1397 <t139754@gmail.com>
2022-03-09 16:56:43 +0900

    22.03.09 백준 알고리즘

---
+++ PycharmProjects/boj/자료 구조/1874_스택수열.py
@@ -0,0 +1,29 @@
+n = int(input())
+stack = []
+nums = [int(input()) for _ in range(n)]
+a = 0
+i = 1
+answer = []
+flag = 0
+
+while True:
+
+    while stack and stack[-1] == nums[a]:
+        a += 1
+        stack.pop()
+        answer.append("-")
+
+    if i > n and not stack:
+         break
+    elif i > n and stack:
+        flag = 1
+        break
+
+    stack.append(i)
+    answer.append("+")
+    i += 1
+
+if flag:
+    print("NO")
+else:
+    print(*answer, sep="\n")
