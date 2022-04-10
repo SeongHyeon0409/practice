93dfc576e7d28513657f92d22cfffec2df6aa799
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-04-10 23:05:33 +0900

    22.04.10 백준 알고리즘

--- PycharmProjects/boj/다이나믹/2565_전깃줄.py
+++ PycharmProjects/boj/다이나믹/2565_전깃줄.py
@@ -8,8 +8,8 @@ l = sorted([list(map(int, input().split())) for _ in range(n)])
 dp = [1] * n
 
 for i in range(n):
-    for j in range(i):
+    for j in range(i): #안겹치는 줄을 찾기위해 두번째 전봇대의 lis를 구해준다.
         if l[i][1] > l[j][1]:
             dp[i] = max(dp[j] + 1, dp[i])
 
-print(n - max(dp))
+print(n - max(dp)) #전체 전깃줄 개수 - 안겹치는 전깃줄 = 최소 없애야 하는 전깃줄
