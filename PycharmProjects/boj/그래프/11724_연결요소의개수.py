782c2179b3248fc637cbcf4d3e316ded8be82fa6
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-04-22 18:39:26 +0900

    22.04.22 백준 알고리즘

---
+++ PycharmProjects/boj/그래프/11724_연결요소의개수.py
@@ -0,0 +1,36 @@
+# 2020.04.22
+# Presented by SeongHyeon0409
+# 그래프
+import sys
+sys.setrecursionlimit(99999)
+input = sys.stdin.readline
+
+n, m = map(int, input().strip().split())
+edges = dict()
+visited = [False] * (n + 1)
+count = 0
+
+for i in range(1, n+1):
+    edges[i] = []
+
+for i in range(m):
+    s ,e = map(int, input().strip().split())
+    edges[s].append(e)
+    edges[e].append(s)
+
+for i in range(1, n + 1):
+    edges[i].sort()
+
+def dfs(edges, start, visited):
+    visited[start] = True
+
+    for i in edges[start]:
+        if not visited[i]:
+            dfs(edges, i, visited)
+
+for i in range(1, len(visited)):
+    if visited[i] == False:
+        count += 1
+        dfs(edges, i, visited)
+
+print(count)
