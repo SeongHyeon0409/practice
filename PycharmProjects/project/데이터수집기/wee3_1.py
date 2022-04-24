ab902c816b2564905739c8d4e71024a84476cbeb
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-04-25 03:17:00 +0900

    웹 크롤링 연습

--- PycharmProjects/project/데이터수집기/wee3_1.py
+++ PycharmProjects/project/데이터수집기/wee3_1.py
@@ -2,5 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 raw = requests.get("https://tv.naver.com/r")
-print(raw.text)
-print(raw.elapsed)
+#print(raw.text)
+
+html = BeautifulSoup(raw.text, "html.parser")
+#print(html)
+
+clips = html.select("div.inner")
+#print(clips[0])
+
+# 데이터 수집부분
+title = clips[0].select_one("dt.title")
+chn = clips[0].select_one("dd.chn")
+hit = clips[0].select_one("span.hit")
+like = clips[0].select_one("span.like")
+
+# 수집결과 출력부분
+print(title.text.strip())
+print(chn.text.strip())
+print(hit.text.strip())
+print(like.text.strip())
