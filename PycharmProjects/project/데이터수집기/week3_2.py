af7688e4683cf4abc57dacdaf502540690a38353
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-04-25 19:58:35 +0900

    네이버 뉴스 크롤링

--- PycharmProjects/project/데이터수집기/week3_2.py
+++ PycharmProjects/project/데이터수집기/week3_2.py
@@ -3,7 +3,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-raw = requests.get("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%B9%84%ED%8A%B8%EC%BD%94%EC%9D%B8"
+raw = requests.get("https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EB%B9%84%ED%8A%B8%EC%BD%94%EC%9D%B8"
                    , headers={'User-Agent':'Mozilla/5.0'}) #안티 크롤링 피해가기
 html = BeautifulSoup(raw.text, "html.parser")
+
+articles = html.select("ul.list_news > li")
+
+for ar in articles:
+    title = ar.select_one("a.news_tit").text
+    source = ar.select_one("a.info.press").text
+    print(title, source)
+
+#기사 컨테이너 선택자 ul.list_news > li
+#기사 제목 선택자 a.news.tit
+#언론사 선택자 a.info press
 
