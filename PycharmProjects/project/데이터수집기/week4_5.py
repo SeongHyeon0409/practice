a7c04833c130fb2136c2393a29a278a60b306174
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-05-14 23:17:06 +0900

    22.05.14 데이터수집기

--- PycharmProjects/project/데이터수집기/week4_5.py
+++ PycharmProjects/project/데이터수집기/week4_5.py
@@ -7,6 +7,10 @@ from bs4 import BeautifulSoup
 import requests, openpyxl
 from bs4 import BeautifulSoup
 
+wb = openpyxl.Workbook()
+sheet = wb.active
+sheet.append(["제목", "채널", "재생 수", "좋아요 수"])
+
 raw = requests.get("https://tv.naver.com/r")
 html = BeautifulSoup(raw.text, "html.parser")
 
@@ -20,8 +24,15 @@ for cl in clips:
     hit = cl.select_one("span.hit").text.strip()
     like = cl.select_one("span.like").text.strip()
 
+    hit = hit.replace("재생 수", "")
+    like = like.replace("좋아요 수", "")
+
     print("제목", title)
     print("채널명", chn)
     print(hit)
     print(like)
-    print("="*50)
+    print("=" * 50)
+
+    sheet.append([title, chn, hit, like])
+
+wb.save("navertv.xlsx")
