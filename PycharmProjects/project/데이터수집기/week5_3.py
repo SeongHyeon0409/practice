397977abe07fa4de4109e06f0a1d04b59d46dce7
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-05-21 03:43:40 +0900

    22.05.21 데이터수집기

--- PycharmProjects/project/데이터수집기/week5_3.py
+++ PycharmProjects/project/데이터수집기/week5_3.py
@@ -17,3 +17,18 @@ for m in movie:
     print("="*50)
     print(title.text)
 
+    url = "https://movie.naver.com" + title.attrs["href"]
+    print(url)
+
+    raw_each = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    html_each = BeautifulSoup(raw_each.text, 'html.parser')
+
+    # 컨테이너(댓글) 수집
+    reply = html_each.select("div.score_result li")
+    print("-" * 50)
+    print("평점과 댓글:")
+    for r in reply:
+        # 평점, 댓글 수집/출력
+        score = r.select_one("div.star_score em").text
+        reply = r.select_one("div.score_reple p").text
+        print(score, reply)
