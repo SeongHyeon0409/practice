fa1ee8b6976c975770eab540ce0bf4d10656d78e
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-05-29 15:41:36 +0900

    22.05.29 백준 알고리즘

---
+++ PycharmProjects/boj/수학/20492_세금.py
@@ -0,0 +1,6 @@
+# 2020.05.26
+# Presented by SeongHyeon0409
+
+n = int(input())
+
+print(int(n/100 * 78), int((n/100 * 80) + (n/100 * 20)/100 * 78))
---
+++ PycharmProjects/boj/수학/24736_Football Scoring.py
@@ -0,0 +1,6 @@
+# 2020.05.26
+# Presented by SeongHyeon0409
+
+for i in range(2):
+    a, b, c, d, e = map(int, input().split())
+    print(a * 6 + b * 3 + c * 2 + d * 1 + e * 2, end=' ')
