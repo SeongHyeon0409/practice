1f4d216bcc18686d9d8a02303a1e48b7fae7e4bf
DESKTOP-019BCUV\seonghyeon <t139754@gmail.com>
2022-06-19 18:26:02 +0900

    22.06.19 백준 알고리즘

--- PycharmProjects/gui_basic/11_messagebox.py
+++ PycharmProjects/gui_basic/11_messagebox.py
@@ -17,13 +17,33 @@ def error():
 def okcancel():
     msgbox.askokcancel("확인 / 취소", "해당 좌석은 유아동반석입니다. 예매하시겠습니까?")
 
-def okcancel():
-    msgbox.askokcancel("확인 / 취소", "해당 좌석은 유아동반석입니다. 예매하시겠습니까?")
+def retrycancel():
+    msgbox.askretrycancel("재시도 / 취소", "일시적인 오류입니다. 다시 시도하시겠습니까?")
+
+def yesno():
+    msgbox.askyesno("예 / 아니오", "일시적인 오류입니다. 다시 시도하시겠습니까?")
+
+def yesnocancel():
+    response = msgbox.askyesnocancel(title=None, message="예매 내역이 저장 x \n저장 후 프로그램 종료?")
+    # 네 : 저장 후 종료
+    # 아니오 : 저장 x 종료
+    # 취소 : 프로그램 종료 취소
+    print("응답", response)
+    if response == 1:
+        print("예")
+    elif response == 0:
+        print("아니오")
+    else:
+        print("취소")
 
 Button(root, command=info, text="알림").pack()
-Button(root, command=info, text="경고").pack()
-Button(root, command=info, text="에러").pack()
+Button(root, command=warn, text="경고").pack()
+Button(root, command=error, text="에러").pack()
+
 Button(root, command=okcancel, text="확인 취소").pack()
+Button(root, command=retrycancel, text="재시도 취소").pack()
+Button(root, command=yesno, text="예 아니오").pack()
+Button(root, command=yesnocancel, text="예 아니오 취소").pack()
 
 
 root.mainloop()
