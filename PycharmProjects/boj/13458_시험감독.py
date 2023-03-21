565b44ff2cb3032fe54846324517497b4154081a
Seonghyeon0409 <t139754@gmail.com>
2023-03-21 14:15:55 +0900

    boj 2023-03-21

--- PycharmProjects/boj/13458_시험감독.py
+++ PycharmProjects/boj/13458_시험감독.py
@@ -1,47 +1,14 @@
-# 2023.03.20
+# 2023.02.09
 # Written by SeongHyeon0409
 
-import copy
-def printm(m):
-    for i in m:
-        print(*i)
+n = int(input())
+a = list(map(int, (input().split())))
+b ,c = map(int, input().split())
 
-r, c = map(int, input().split())
-
-maps = []
-ans = 0
-
-dx = [1, 1, 1]
-dy = [-1, 0, 1]
-
-for i in range(r):
-    maps.append(list(input().strip()))
-
-for i in range(r):
-    cy, cx = i, 0
-    flag = True
-    mapl = []
-
-    while cx != c-1:
-        flag1 = False
-        for j in range(3):
-            ny, nx = cy + dy[j], cx + dx[j]
-
-            if 0 <= ny < r and 0 <= nx < c and maps[ny][nx] == '.':
-                mapl.append([ny, nx])
-                cy, cx = ny, nx
-                flag1 = True
-                break
-
-        if flag1:
-            continue
-
-        flag = False
-        break
-
-    if flag:
-        for _ in mapl:
-            maps[_[0]][_[1]] = 'x'
-        ans += 1
-
-print(ans)
+answer = n
+for i in a:
+    if i <= b:
+        continue
+    remainder = i - b
+    answer += (remainder // c if remainder % c == 0 else remainder // c + 1)
+print(answer)
--- PycharmProjects/boj/3109_빵집.py
+++ PycharmProjects/boj/3109_빵집.py
@@ -1,3 +1,29 @@
-# 2023.03.13
+# 2023.03.20
 # Written by SeongHyeon0409
 
+def solve(i, j):
+    if j == c - 1:
+        return True
+
+    for d in dy:
+        if 0 <= i + d < r and maps[i + d][j + 1] == '.' and not visit[i + d][j + 1]:
+            visit[i + d][j + 1] = True
+            if solve(i + d, j + 1):
+                return True
+    return False
+
+r, c = map(int, input().split())
+visit = [[False] * c for _ in range(r)]
+maps = []
+for i in range(r):
+    maps.append(list(input().strip()))
+
+ans = 0
+dy = [-1, 0, 1]
+
+for i in range(r):
+    if maps[i][0] == '.':
+        if solve(i, 0):
+            ans += 1
+
+print(ans)
