4920e5950dd177437593d1eff65129e41d8455eb
Seonghyeon0409 <t139754@gmail.com>
2024-07-12 21:10:03 +0900

    js

--- jsproject/boj/1000.js
+++ jsproject/boj/1000.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "boj/input.txt";
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().split(" ").map(v => Number(v));
 
 
